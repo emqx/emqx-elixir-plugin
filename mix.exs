5079fad0f95e6c35e7f7cfd307bea40f418b5f6f
Feng Lee <feng@emqtt.io>
2017-12-25 17:29:00 +0800

    Version 2.3.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EmqElixirPlugin.Mixfile do
   def project do
     [
       app: :emq_elixir_plugin,
-      version: "2.3.0",
+      version: "2.3.2",
       elixir: "~> 1.5-dev",
       start_permanent: Mix.env == :prod,
       deps: deps()
