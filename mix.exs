341e6ed97980aab4f988dbb9740c3c8fd51d915b
Gilbert Wong <gilbertwong96@icloud.com>
2019-05-28 17:42:11 +0800

    Fix compilation error

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EmqxElixirPlugin.Mixfile do
     [
       app: :emqx_elixir_plugin,
       version: "2.3.2",
-      elixir: "~> 1.5-dev",
+      elixir: "~> 1.7",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
@@ -22,6 +22,16 @@ defmodule EmqxElixirPlugin.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:cuttlefish, github: "emqx/cuttlefish", tag: "exs-3.0.0", manager: :rebar3, override: true},
+      {:emqx, github: "emqx/emqx", branch: branch()}
     ]
+  end
+
+  defp branch do
+     cur_branch = :os.cmd('git branch | grep -e \'^*\' | cut -d\' \' -f 2') -- '\n'
+     to_string(case :lists.member(cur_branch, ['master', 'develop']) do
+                  true -> cur_branch
+                  false -> 'develop'
+              end)
   end
 end
